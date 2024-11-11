97abba3532abc41d0f7532aa8e952a1ee03977ae
Shaneisia Bailey <shaneisiabaileywhs@gmail.com>
2024-11-11 15:34:27 -0500

    Create and join watch party buttons layout

---
+++ app/src/main/java/com/example/rewatchparty/CreateOrJoinWPActivity.kt
@@ -0,0 +1,17 @@
+package com.example.rewatchparty
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+
+class CreateOrJoinWPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_or_join_wpactivity)
+
+    }
+}
