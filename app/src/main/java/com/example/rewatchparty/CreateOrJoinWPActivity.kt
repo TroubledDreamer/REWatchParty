2c5e22422da74bb897d4ea55ada007fa59a31597
Shaneisia Bailey <shaneisiabaileywhs@gmail.com>
2024-11-13 16:18:55 -0500

    Create and join watch party buttons layout

--- app/src/main/java/com/example/rewatchparty/CreateOrJoinWPActivity.kt
+++ app/src/main/java/com/example/rewatchparty/CreateOrJoinWPActivity.kt
@@ -1,17 +1,26 @@
 package com.example.rewatchparty
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.ImageButton
+import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+
 
 class CreateOrJoinWPActivity : AppCompatActivity() {
+
+    private lateinit var CreateRoomButton: ImageButton;
+    private lateinit var JoinRoomButton: ImageButton;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_or_join_wpactivity)
 
+//        CreateRoomButton = findViewById(R.id.CreateRoomButton)
     }
 }
